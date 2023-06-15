eddc52a0f333017b197972c8f5deccbd91063191
Rodion Otyrba <19rodion97@gmail.com>
2023-06-15 20:02:55 +0300

    choseobject

--- js/GraphicObject.js
+++ js/GraphicObject.js
@@ -3,22 +3,34 @@ class GraphicObject {
     y;
     width = 25;
     height = 25;
+    img = new Image();
+    chose = false;
     constructor(x, y) {
         this.x = x - this.width / 2;
         this.y = y - this.height / 2;
+        
     }
     draw(ctx) {
-        ctx.fillRect(this.x, this.y, 25 ,25);
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+        if (this.chose) {
+            ctx.strokeStyle = "green";
+            ctx.strokeRect(this.x, this.y, this.width, this.height);
+        }
     }
-    rename() { }
-    move() { }
-    delete() { }
+    chosen() {
+        if (this.chose) {
+            this.chose = false;
+        } else {
+            this.chose = true;
+        }
+    }
 }
 
 class Chair extends GraphicObject {
     constructor(x, y, name) {
         super(x, y);
         this.name = name;
+        this.img.src = "../media/chair-icon.svg";
     }
     showInfo() {
         alert(123);
--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ class StandEditor {
     graphicObjectslist = []; // Массив графических обьектов 
     createObjectt = null;
     selectObject = null;
+    canvasWidth;
+    canvasHeight;
 
     constructor(graphicObjectslist) {
         if (graphicObjectslist === undefined) {
@@ -14,58 +16,102 @@ class StandEditor {
         } else {
             this.graphicObjectslist = graphicObjectslist;
         }
-
     }
 
     draw() {
         if (this.graphicObjectslist.length !== 0) {
-            this.ctx.clearRect(0, 0, 500, 400);
+            this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
             this.graphicObjectslist.forEach(object => {
                 object.draw(this.ctx);
             });
-            console.log('Данные подгружены');
         }
     }
 
+    sizeCanvas() {
+        this.canvasWidth = window.innerWidth;
+        this.canvasHeight = window.innerHeight - 85;
+        this.canvas.width = this.canvasWidth;
+        this.canvas.height = this.canvasHeight;
+
+        console.log(this.canvasWidth);
+    }
+
+
+    // Одиночное нажатие на канвас
     clickOnCanvas() {
         if (this.createObjectt != null) {
             this.graphicObjectslist.push(this.createObjectt(this.m_x, this.m_y));
+            if (this.selectObject != null) {
+                this.selectObject.chosen();
+                this.selectObject = null;
+            }
             this.draw();
         }
-        if (this.selectObject == true) {
-            this.graphicObjectslist.forEach(graphicObject => {
-                if ((this.m_x >= graphicObject.x) &&
-                    (this.m_y >= graphicObject.y) &&
-                    (this.m_x <= graphicObject.width + graphicObject.x) &&
-                    (this.m_y <= graphicObject.height + graphicObject.y)
-                );
-            });
+
+        if (this.createObjectt == null) {
+            if (this.selectObject == null) {
+                this.graphicObjectslist.forEach(graphicObject => {
+                    if ((this.m_x >= graphicObject.x) &&
+                        (this.m_y >= graphicObject.y) &&
+                        (this.m_x <= graphicObject.width + graphicObject.x) &&
+                        (this.m_y <= graphicObject.height + graphicObject.y)
+                    ) {
+                        this.selectObject = graphicObject;
+                        this.selectObject.chosen();
+                        this.draw();
+                    };
+                });
+            } else if (this.selectObject != null) {
+                this.selectObject.chosen();
+                this.selectObject = null;
+                this.draw();
+            }
         }
         this.createObjectt = null;
-        this.selectObject = true;
     }
 
-    choseObject() {
+    moveObject() {
 
     }
 
     start() {
         this.canvas = document.getElementById('workspace-canvas');
+
+        this.canvasWidth = window.innerWidth;
+        this.canvasHeight = window.innerHeight - 85;
+        this.canvas.width = this.canvasWidth;
+        this.canvas.height = this.canvasHeight;
+
+        window.onresize = () => {
+            this.canvasWidth = window.innerWidth;
+            this.canvasHeight = window.innerHeight - 85;
+            this.canvas.width = this.canvasWidth;
+            this.canvas.height = this.canvasHeight;
+            this.draw();
+        };
+
         this.ctx = this.canvas.getContext('2d');
 
         this.canvas.addEventListener('mousemove', (e) => {
             this.m_x = e.pageX - e.target.offsetLeft;
             this.m_y = e.pageY - e.target.offsetTop;
         });
+
         this.canvas.addEventListener('click', () => {
             this.clickOnCanvas();
         })
 
+        this.canvas.addEventListener('dblclick', () => {
+            this.choseObject();
+        })
+
         this.chairButton = document.getElementById('chairButton');
         this.tableButton = document.getElementById('tableButton');
+
         this.chairButton.addEventListener('click', () => {
             this.createObjectt = (x, y) => new Chair(x, y);
         })
+
         console.log('Приложение запущено');
     }
 
